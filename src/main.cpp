b0f07d95950bf0f4afe1a1bd7c4373c7fb76b19d
h3nnn4n <uber.renan@gmail.com>
2019-05-15 08:50:14 -0300

    Keyboard spam + blinky

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,34 @@
 
 extern "C" int main(void)
 {
-    // To use Teensy 3.0 without Arduino, simply put your code here.
-    // For example:
-
-    pinMode(13, OUTPUT);
+    setup();
 
     while (1)
     {
-        digitalWriteFast(13, HIGH);
-        delay(500);
-        digitalWriteFast(13, LOW);
-        delay(500);
+        loop();
     }
+}
+
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    // TODO: Make the linter happy
+    // TODO: Fix KEY_* type to suppress warnings
+    keyboard_keys[0] = KEY_A;
+    usb_keyboard_send();
+
+    delay(5);
+
+    keyboard_keys[0] = 0;
+    usb_keyboard_send();
+
+    digitalWriteFast(13, HIGH);
+    delay(500);
+
+    digitalWriteFast(13, LOW);
+    delay(500);
 }
