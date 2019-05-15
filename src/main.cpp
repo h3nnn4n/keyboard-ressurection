dc8d86e74ec658bcd65ee78500f03665aa2382f6
h3nnn4n <uber.renan@gmail.com>
2019-05-15 08:40:25 -0300

    No need to support arduino stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,16 @@
 
 extern "C" int main(void)
 {
-#ifdef USING_MAKEFILE
+    // To use Teensy 3.0 without Arduino, simply put your code here.
+    // For example:
 
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
+    pinMode(13, OUTPUT);
 
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
-
-
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
-#endif
+    while (1)
+    {
+        digitalWriteFast(13, HIGH);
+        delay(500);
+        digitalWriteFast(13, LOW);
+        delay(500);
+    }
 }
-
