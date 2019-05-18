e29f093a385cb3c1ed37ec516f38dd213eaaf82f
h3nnn4n <uber.renan@gmail.com>
2019-05-18 10:24:24 -0300

    Add guards for config.h

--- src/config.h
+++ src/config.h
@@ -1,6 +1,11 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
 const int column_pins[] = {10, 11, 12};
 const int row_pins[] = {14, 15, 16};
 const int n_columns = 3;
 const int n_rows = 3;
 
 const int key_map[] = {KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H, KEY_I};
+
+#endif
